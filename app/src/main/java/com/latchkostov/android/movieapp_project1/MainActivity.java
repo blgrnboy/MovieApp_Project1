80aee0e07d1a5a114140fbabfdbd9eaa33500b62
blgrnboy <latch.kostov@gmail.com>
2017-03-09 23:08:52 -0600

    Add picasso and internet permissions

--- app/src/main/java/com/latchkostov/android/movieapp_project1/MainActivity.java
+++ app/src/main/java/com/latchkostov/android/movieapp_project1/MainActivity.java
@@ -2,12 +2,21 @@ package com.latchkostov.android.movieapp_project1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.iv_test);
+
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
     }
 }
