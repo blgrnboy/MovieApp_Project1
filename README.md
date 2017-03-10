bd8d1fde61c984a78f4d005ab44af0527ca6fd04
blgrnboy <latch.kostov@gmail.com>
2017-03-10 00:06:27 -0600

    read api key.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # MovieApp_Project1
 API KEY
-  \MovieApp_Project1\app\src\main\res\values should contain a text file called tmdb_apikey.txt.  The program reads the key from that file.
+  \MovieApp_Project1\app\src\main\res\raw should contain a text file called tmdb_apikey.txt.  The program reads the key from that file.
--- app/src/main/java/com/latchkostov/android/movieapp_project1/MainActivity.java
+++ app/src/main/java/com/latchkostov/android/movieapp_project1/MainActivity.java
@@ -2,9 +2,18 @@ package com.latchkostov.android.movieapp_project1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.iv_test);
 
         Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        String key = getApiKey();
+        Log.d("key", key);
+    }
+
+    String getApiKey() {
+        String key = null;
+        InputStream is = getResources().openRawResource(R.raw.tmdb_apikey);
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            key = br.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return key;
     }
 }
